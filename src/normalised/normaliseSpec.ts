4ce86aa1664a5bb08720f2fbb5f98dabd72eea2c
Gordon Leigh <gordon.leigh@futurematik.co.uk>
2020-01-28 09:21:21 +0000

    don't escape attribute names

--- src/normalised/normaliseSpec.ts
+++ src/normalised/normaliseSpec.ts
@@ -3,8 +3,6 @@ import { TypeDefinition } from './TypeDefinition';
 import { TypeSource } from './TypeSource';
 import { normaliseTypeSpec } from './normaliseTypeSpec';
 import { parseTypeName } from './TypeName';
-import { StringMap } from '../model/StringMap';
-import { AttributeSpecification } from '../model/AttributeSpecification';
 import { makeResolver } from './NameResolver';
 
 export function normaliseSpec(spec: CfnSpecification): TypeDefinition[] {
@@ -31,7 +29,7 @@ export function normaliseSpec(spec: CfnSpecification): TypeDefinition[] {
       normaliseTypeSpec(
         { name: 'Attributes', namespace: name },
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        { Properties: escapeAttribs(resourceSpec.Attributes!) },
+        { Properties: resourceSpec.Attributes! },
         TypeSource.AttributeType,
         resolve,
       ),
@@ -40,20 +38,4 @@ export function normaliseSpec(spec: CfnSpecification): TypeDefinition[] {
   return [...propTypes, ...resourceTypes, ...attribTypes].sort((a, b) =>
     a.name > b.name ? 1 : a.name === b.name ? 0 : -1,
   );
-}
-
-function escapeAttribs(
-  spec: StringMap<AttributeSpecification>,
-): StringMap<AttributeSpecification> {
-  return Object.entries(spec).reduce(
-    (a, [k, v]) => ({ ...a, [escapeName(k)]: v }),
-    {},
-  );
-}
-
-function escapeName(name: string): string {
-  if (!/^[A-Za-z_][A-Za-z0-9_]*$/.test(name)) {
-    return `"${name}"`;
-  }
-  return name;
 }
